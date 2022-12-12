efb99cd5c316e28911d8e3cc667916b93dab0a46
ryosangels24@gmail.com <ryosangels24@gmail.com>
2022-12-12 17:54:11 -0500

    Add S13

---
+++ S13script.js
@@ -0,0 +1,21 @@
+let app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope) {
+  $scope.distance = 0;
+  $scope.calcularTarifa = function () {
+    if ($scope.distance <= 300) {
+      return 250;
+    } else if ($scope.distance <= 1000) {
+      return 250 + 30 * ($scope.distance - 300);
+    } else {
+      return 250 + 30 * (1000 - 300) + 20 * ($scope.distance - 1000);
+    }
+
+    
+  };
+
+  $scope.descuento=function(){
+    if ($scope.calcularTarifa()>500){
+        return 0.9*($scope.calcularTarifa())
+    }
+  }
+});
